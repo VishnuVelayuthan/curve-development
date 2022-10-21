de120900057347641ea6c9491fe2f08a7e5271da
Vishnu  Velayuthan <vishnuvelayuthan@Vishnus-MacBook-Pro.local>
2022-10-21 14:28:00 -0700

    Callable function to receive an image from the 3D simulation

--- input-pose-extractor/ImageExtractor.py
+++ input-pose-extractor/ImageExtractor.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import InputPoseExtractor 
 import os
+import pdb
 
 from habitat_sim.utils.data import ImageExtractor
 
@@ -24,34 +25,41 @@ def display_sample(sample):
     plt.show()
 
 
+def get_image_at(x, y, theta, display=False):
+
 # Get input for image (x, y, theta) and writing vars to file
-var_file = open("var-file.txt", "w")
-var_file.write(input("Input x y theta: ")) # gets x y theta all seperated by a space
-var_file.close();
+    var_file = open("var-file.txt", "w")
+    var_file.write(str(x) + " " + str(y) + " " + str(theta)) # gets x y theta all seperated by a space
+    var_file.close();
 
-scene_filepath = "../data/apartment_1.glb"
+    scene_filepath = "../data/apartment_1.glb"
 
-extractor = ImageExtractor(
-    scene_filepath,
-    img_size=(512, 512),
-    output=["rgba", "depth", "semantic"],
-    pose_extractor_name="input_pose_extractor",
-    split=(100,0)
-)
+    extractor = ImageExtractor(
+        scene_filepath,
+        img_size=(512, 512),
+        output=["rgba", "depth", "semantic"],
+        pose_extractor_name="input_pose_extractor",
+        split=(100,0)
+    )
 
 # remove temp var file 
-os.remove("var-file.txt");
+    os.remove("var-file.txt");
 
 # Use the list of train outputs instead of the default, which is the full list
 # of outputs (test + train)
-extractor.set_mode('train')
+    extractor.set_mode('train')
 
 # Index in to the extractor like a normal python list
-sample = extractor[0]
+    sample = extractor[0]
 
-# Or use slicing
-display_sample(sample)
+    if (display): display_sample(sample)
 
 # Close the extractor so we can instantiate another one later
 # (see close method for detailed explanation)
-extractor.close()
+    extractor.close()
+
+    return sample
+
+
+if __name__ == "__main__" :
+    get_image_at(40, 40, 0, True)
